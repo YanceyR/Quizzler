1cca158e6224e9bc761eb95241d492267df9b009
YanceyR <yancey.reid@gmail.com>
2017-09-22 01:45:17 -0400

    Update UI labels when button is pressed
    - Proceed to next question in QuestionBank  when answer is pressed.
    - Update score, current question number, and question text UI elements
    - Restart quiz when last question is answered

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
+    var score : Int = 0
+    var progressNumber : Int = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -20,34 +24,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
-
-
+    
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        pickedAnswer = (sender.tag == 1 ? true : false)
+        checkAnswer()
     }
-    
-    
-    func updateUI() {
-      
-    }
-    
-
-    func nextQuestion() {
-        
-    }
-    
     
     func checkAnswer() {
+        if (allQuestions.list[questionNumber].answer == pickedAnswer) {
+            score += 1
+        }
+        nextQuestion()
+    }
+    
+    func nextQuestion() {
+        questionNumber += 1
+        progressNumber += 1
         
+        if (questionNumber > allQuestions.list.count - 1) {
+            startOver()
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(progressNumber + 1)/ \(allQuestions.list.count)"
     }
     
     
     func startOver() {
-       
+        questionNumber = 0
+        score = 0
+        progressNumber = 0
     }
-    
-
-    
 }
